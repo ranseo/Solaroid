0da751fabe01ad25cf623c555e6c61f53ad1556e
ranseo <55490534+ranseo@users.noreply.github.com>
2022-02-05 06:18:20 +0900

    Update BottomNavigation & Menu
    for navigationg fragment ( between gallery and frame Fragment)

--- app/src/main/java/com/example/solaroid/MainActivity.kt
+++ app/src/main/java/com/example/solaroid/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.solaroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.solaroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = binding.navHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.botNaivMenu.setupWithNavController(navController)
+
+
     }
 }
