741a5a863819060c6bbec0a5fb3c63417437ce13
ranseo <slcksseo@gmail.com>
2022-05-10 21:47:59 +0900

    navigationView Custom 성공,
    이제 Login화면 시작시, 회원가입과 별명 및 프로필에 쓰일 사진들 설정하는 프래

--- app/src/main/java/com/example/solaroid/NavigationViewModel.kt
+++ app/src/main/java/com/example/solaroid/NavigationViewModel.kt
@@ -2,21 +2,30 @@ package com.example.solaroid
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.example.solaroid.firebase.FirebaseManager
+import com.example.solaroid.login.FirebaseAuthLiveData
 
 class NavigationViewModel : ViewModel() {
     val firebaseAuth = FirebaseManager.getAuthInstance()
+
 
     private val _naviToLoginAct = MutableLiveData<Event<Any?>>()
     val naviToLoginAct : LiveData<Event<Any?>>
         get() = _naviToLoginAct
 
+    val userEmail = Transformations.map(FirebaseAuthLiveData()){
+        it?.let{ user ->
+            user.email
+        }
+    }
+
+
+
     fun navigateToLoginAct() {
         _naviToLoginAct.value = Event(Unit)
     }
-
-    fun getUserEmail() = firebaseAuth.currentUser?.email.toString()
 
 
 }
