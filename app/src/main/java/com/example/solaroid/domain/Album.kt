14676f2c2bddf6d66d0894a4bab069584efcd922
ranseo <slcksseo@gmail.com>
2022-07-01 20:52:58 +0900

    ..

--- app/src/main/java/com/example/solaroid/domain/Album.kt
+++ app/src/main/java/com/example/solaroid/domain/Album.kt
@@ -1,6 +1,7 @@
 package com.example.solaroid.domain
 
 data class Album(
+    val id:String,
     var name: String,
     var participants: List<Profile>,
     val photoTickets : MutableList<PhotoTicket>,
