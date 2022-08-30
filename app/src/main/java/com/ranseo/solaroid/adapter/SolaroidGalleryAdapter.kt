0025f85d8cf6ebf2fb0f0200430f322db3b3ad78
ranseo <55490534+ranseo@users.noreply.github.com>
2022-08-30 15:39:25 +0900

    GalleryFragment 의 listitem을 long Click했을 때
    gallery edit fragment로 진입할 수 있도록.

--- app/src/main/java/com/ranseo/solaroid/adapter/SolaroidGalleryAdapter.kt
+++ app/src/main/java/com/ranseo/solaroid/adapter/SolaroidGalleryAdapter.kt
@@ -1,32 +1,58 @@
 package com.ranseo.solaroid.adapter
 
 
+import android.app.Application
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.ranseo.solaroid.R
 import com.ranseo.solaroid.models.domain.PhotoTicket
 import com.ranseo.solaroid.databinding.ListItemSolaroidGalleryBinding
+import com.ranseo.solaroid.databinding.ListItemSolaroidGalleryLongClickBinding
+import java.lang.ClassCastException
 
 
-class SolaroidGalleryAdapter(val clickListener: OnClickListener) :
-    ListAdapter<PhotoTicket, SolaroidGalleryAdapter.PhotoViewHolder>(PhotoTicketDiffCallback()) {
+const val VIEW_TYPE_NORMAL_GALLERY = 0
+const val VIEW_TYPE_LONGCLICK_GALLERY = 1
+
+class SolaroidGalleryAdapter(val clickListener: OnClickListener, val application: Application) :
+    ListAdapter<GalleryListDataItem, RecyclerView.ViewHolder>(GalleryDataItemCallback()) {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-        return PhotoViewHolder.from(parent)
+    override fun getItemViewType(position: Int): Int {
+        return when(getItem(position)) {
+            is GalleryListDataItem.NormalGalleryDataItem -> VIEW_TYPE_NORMAL_GALLERY
+            is GalleryListDataItem.LongClickGalleryDataItem -> VIEW_TYPE_LONGCLICK_GALLERY
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            VIEW_TYPE_NORMAL_GALLERY ->  NormalViewHolder.from(parent)
+            VIEW_TYPE_LONGCLICK_GALLERY -> LongClickViewHolder.from(parent)
+            else -> throw ClassCastException("UNKNOWN_VIEW_TYPE_${viewType}")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+            is NormalViewHolder -> {
+                val item = getItem(position) as GalleryListDataItem.NormalGalleryDataItem
+                holder.bind(item.photoTicket, clickListener)
+            }
+            is LongClickViewHolder -> {
+                val item = getItem(position) as GalleryListDataItem.LongClickGalleryDataItem
+                holder.bind(item.photoTicket,application)
+            }
+        }
     }
 
 
-    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item, clickListener)
-        //holder.binding.executePendingBindings()
-    }
-
-
-
-        class PhotoViewHolder(val binding: ListItemSolaroidGalleryBinding) : RecyclerView.ViewHolder(binding.root) {
+    class NormalViewHolder(val binding: ListItemSolaroidGalleryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: PhotoTicket, clickListener: OnClickListener) {
             this.binding.photoTicket = item
@@ -34,14 +60,78 @@ class SolaroidGalleryAdapter(val clickListener: OnClickListener) :
         }
 
         companion object {
-            fun from(parent: ViewGroup): PhotoViewHolder {
+            fun from(parent: ViewGroup): NormalViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ListItemSolaroidGalleryBinding.inflate(layoutInflater, parent, false)
-                return PhotoViewHolder(binding)
+                return NormalViewHolder(binding)
             }
         }
     }
 
+    class LongClickViewHolder(val binding: ListItemSolaroidGalleryLongClickBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: PhotoTicket, application:Application) {
+            this.binding.photoTicket = item
+
+            binding.flag = false
+            val lambda : (view: View) -> Unit = {
+                try {
+                    binding.flag = !binding.flag
+                    if (binding.flag) {
+                        binding.photoLayout.background =
+                            application!!.getDrawable(R.drawable.border_line_grey)
+                    } else {
+                        binding.photoLayout.background =
+                            application!!.getDrawable(R.color.grey_transper)
+                    }
+                } catch (error:Exception){
+
+                }
+            }
+
+            binding.photoLayout.setOnClickListener(lambda)
+            binding.btnCheck.setOnClickListener(lambda)
+            binding.btnEmpty.setOnClickListener(lambda)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): LongClickViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemSolaroidGalleryLongClickBinding.inflate(layoutInflater, parent, false)
+                return LongClickViewHolder(binding)
+            }
+        }
+    }
+
+
+
+}
+
+class GalleryDataItemCallback() : DiffUtil.ItemCallback<GalleryListDataItem>() {
+    override fun areItemsTheSame(
+        oldItem: GalleryListDataItem,
+        newItem: GalleryListDataItem
+    ): Boolean = oldItem == newItem
+
+    override fun areContentsTheSame(
+        oldItem: GalleryListDataItem,
+        newItem: GalleryListDataItem
+    ): Boolean = oldItem.id == newItem.id
+}
+
+sealed class GalleryListDataItem() {
+    abstract val id: String
+
+    class NormalGalleryDataItem(val photoTicket: PhotoTicket) : GalleryListDataItem() {
+        override val id: String
+            get() = photoTicket.id
+    }
+
+    class LongClickGalleryDataItem(val photoTicket: PhotoTicket) : GalleryListDataItem() {
+        override val id: String
+            get() = photoTicket.id
+    }
 }
 
 
