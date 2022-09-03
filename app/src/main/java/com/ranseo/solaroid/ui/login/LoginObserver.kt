62f6e5735b294c7fcef02399a2b4f136a958c3d2
ranseo <slcksseo@gmail.com>
2022-09-03 09:19:22 +0900

    Google 계정으로 OneTap Login 구현하는 중
     startIntentSenderForResult() 가 deprecated 되어 다른 방안 찾는중

--- app/src/main/java/com/ranseo/solaroid/ui/login/LoginObserver.kt
+++ app/src/main/java/com/ranseo/solaroid/ui/login/LoginObserver.kt
@@ -2,21 +2,24 @@ package com.ranseo.solaroid.ui.login
 
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import com.google.android.gms.auth.api.identity.BeginSignInResult
 import com.ranseo.solaroid.ui.login.viewmodel.SolaroidProfileViewModel
 
 class LoginObserver(val registry: ActivityResultRegistry) : DefaultLifecycleObserver {
-    lateinit var getContent: ActivityResultLauncher<String>
+    lateinit var getContent: ActivityResultLauncher<IntentSenderRequest>
     override fun onCreate(owner: LifecycleOwner) {
-        getContent = registry.register("KEY", ActivityResultContracts.StartIntentSenderForResult) {
+        getContent = registry.register("KEY", ActivityResultContracts.StartIntentSenderForResult()) {
 
         }
     }
 
-    fun selectImage() {
-        getContent.launch("image/*")
+    fun selectImage(result:BeginSignInResult) {
+        val intentSenderResult = IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
+        getContent.launch(intentSenderResult)
 
     }
 }
